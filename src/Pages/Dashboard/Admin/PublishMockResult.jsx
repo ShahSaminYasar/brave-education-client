9ba1ab0cfb784f2878b71928c810c36da35248b6
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2024-09-13 22:46:21 +0600

    Fixed: Publish Mock Result page colors

--- src/Pages/Dashboard/Admin/PublishMockResult.jsx
+++ src/Pages/Dashboard/Admin/PublishMockResult.jsx
@@ -2,12 +2,18 @@ import moment from "moment";
 import Title from "../../../Components/Title";
 import useAxios from "../../../hooks/useAxios";
 import toast from "react-hot-toast";
+import { MdDateRange } from "react-icons/md";
+import { useState } from "react";
 
 const PublishMockResult = () => {
   const axios = useAxios();
 
+  const [publishing, setPublishing] = useState(false);
+
   const publishResult = async (e) => {
     event.preventDefault();
+    setPublishing(true);
+
     const data = {
       id: e.target.id.value || "",
       number: e.target.number.value,
@@ -19,13 +25,27 @@ const PublishMockResult = () => {
       overall: e.target.overall.value,
       date: moment(e.target.date.value).format("DD-MM-YYYY"),
     };
+
     const res = await axios.post("/mock-result", {
       data,
       token: localStorage.getItem("be_admin"),
     });
+
     if (res?.data?.message === "success") {
+      e.target.id.value = "";
+      e.target.number.value = "";
+      e.target.name.value = "";
+      e.target.listening.value = "";
+      e.target.reading.value = "";
+      e.target.writing.value = "";
+      e.target.speaking.value = "";
+      e.target.overall.value = "";
+
+      setPublishing(false);
+
       return toast.success(`Result published successfully`);
     } else {
+      setPublishing(false);
       return toast.error(res?.data?.message || "Failed, please retry");
     }
   };
@@ -45,7 +65,7 @@ const PublishMockResult = () => {
             type="text"
             placeholder="Student's ID"
             name="id"
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
@@ -58,19 +78,19 @@ const PublishMockResult = () => {
             placeholder="Student's Phone Number"
             name="number"
             required
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
         <div className="flex flex-col gap-1 justify-start">
           <span className="text-slate-800 text-md font-semibold">
-            Student's Name
+            Student&apos;s Name
           </span>
           <input
             type="text"
             placeholder="Student's Name"
             name="name"
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
@@ -78,13 +98,16 @@ const PublishMockResult = () => {
           <span className="text-slate-800 text-md font-semibold">
             Test Date
           </span>
-          <input
-            type="date"
-            placeholder="Date on which the test was attended"
-            name="date"
-            required
-            className="input input-bordered w-full max-w-xs"
-          />
+          <div className="w-full relative">
+            <input
+              type="date"
+              placeholder="Date on which the test was attended"
+              name="date"
+              required
+              className="input bg-white input-bordered w-full max-w-xs"
+            />
+            <MdDateRange className="text-lg text-slate-700 absolute top-[50%] -translate-y-[50%] right-3 pointer-events-none" />
+          </div>
         </div>
 
         <div className="flex flex-col gap-1 justify-start">
@@ -96,7 +119,7 @@ const PublishMockResult = () => {
             placeholder="Listening"
             name="listening"
             required
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
@@ -109,7 +132,7 @@ const PublishMockResult = () => {
             placeholder="Reading Score"
             name="reading"
             required
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
@@ -122,7 +145,7 @@ const PublishMockResult = () => {
             placeholder="Writing Score"
             name="writing"
             required
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
@@ -135,7 +158,7 @@ const PublishMockResult = () => {
             placeholder="Speaking Score"
             name="speaking"
             required
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
@@ -148,12 +171,15 @@ const PublishMockResult = () => {
             placeholder="Overall Score"
             name="overall"
             required
-            className="input input-bordered w-full max-w-xs"
+            className="input bg-white input-bordered w-full max-w-xs"
           />
         </div>
 
-        <button className="btn bg-red-600 border-0 text-white">
-          Publish Result
+        <button
+          disabled={publishing}
+          className="btn bg-red-600 border-0 text-white disabled:bg-red-800"
+        >
+          {publishing ? "Publishing" : "Publish Result"}
         </button>
       </form>
     </section>
